904ccac1ef05771155b8b5b884f4fc15dce8af5b
Nathan Doan on School Computer <nathandoan7222@gmail.com>
2019-11-13 12:19:01 -0800

    functions

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -1,7 +1,38 @@
-function simpleFunction() {
-    console.log('functions should consist of statements designed to perform a single task.')
+window.onload = () => {
+    // All code goes in here.
+    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+    const functionCallsAction = document.getElementById('functionCallsAction');
+            
+    functionCallsAction.onclick = simpleFunction;
+
+    function simpleFunction() {
+        console.log("functions should consist of statements designed to perform a single task.")
+        simpleFunctionDisplay.textContent = "simple";
 }
     
-function functionParameters() {
-    
+function functionParameters(value) {
+    console.log(value);
+}
+
+function functionReturn(addResult) {
+    return "the add function returned " + addResult;
+}
+
+function add(a, b) {
+    const addResult = a + b;
+    console.log('Inside add', addResult);
+    return addResult;
+
+}
+
+simpleFunction();
+
+functionParameters("Many functions take parameters.");
+
+const addResult = add(2, 3);
+console.log('Add Result', addResult);
+const returnValue = functionReturn(addResult);
+
+console.log(returnValue);
+
 }
